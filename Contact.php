013003b78ac1ce7cb8883d6f8bdb563fdc6f270a
lesgourmandisesdeRachel <115095709+lesgourmandisesdeRachel@users.noreply.github.com>
2022-10-17 21:11:27 +0200

    Add files via upload

--- Contact.php
+++ Contact.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
     <title>Aux Gourmandises de Rachel</title>
 </head>
 
-<body>
+<body> -->
     <?php
     $retour = mail("carreau.v@free.fr", "Renseignement ", $_POST['message'], "");
     if ($retour)
         echo '<p>Votre message a bien été envoyé.</p>';
     ?>
-</body>
-</html>
+<!-- </body>
+</html> -->
